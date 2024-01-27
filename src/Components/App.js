64d607116ef13740ed9fccf4a5fc00be7c68065e
Abdo <abdessamadelfathi2018@gmail.com>
2024-01-27 16:52:15 +0100

    React quiz app practicint useReducer

---
+++ src/Components/App.js
@@ -0,0 +1,101 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NexButton from "./NexButton";
+
+const initialState = {
+  questions: [],
+
+  // Loading, Error, Ready, Active, Finished
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    case "active":
+      return { ...state, status: "active" };
+
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+};
+
+export default function App() {
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+
+    // const fetchQuestions = async () => {
+    //   const res = await fetch("http://localhost:8000/questions");
+    //   const data = await res.json();
+
+    //   console.log(data);
+    //   return data;
+    // };
+
+    // fetchQuestions();
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NexButton dispatch={dispatch} answer={answer}></NexButton>
+          </>
+        )}
+      </Main>
+    </div>
+  );
+}
